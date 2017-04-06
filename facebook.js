195d2ff9a55e42ee9d27575b1387daf9994f4dca
aivytran <aivytran@berkeley.edu>
2017-04-06 10:33:28 -0700

    test

--- facebook.js
+++ facebook.js
@@ -24,7 +24,37 @@ const fbMessage = (recipientId, data, cb) => {
       recipient: {
         id: recipientId,
       },
-      message: data,
+      message: {
+  	    "attachment": {
+  		    "type": "template",
+  		    "payload": {
+  				"template_type": "generic",
+  			    "elements": [{
+  					"title": "First card",
+  				    "subtitle": "Element #1 of an hscroll",
+  				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+  				    "buttons": [{
+  					    "type": "web_url",
+  					    "url": "https://www.messenger.com",
+  					    "title": "web url"
+  				    }, {
+  					    "type": "postback",
+  					    "title": "Postback",
+  					    "payload": "Payload for first element in a generic bubble",
+  				    }],
+  			    }, {
+  				    "title": "Second card",
+  				    "subtitle": "Element #2 of an hscroll",
+  				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+  				    "buttons": [{
+  					    "type": "postback",
+  					    "title": "Postback",
+  					    "payload": "Payload for second element in a generic bubble",
+  				    }],
+  			    }]
+  		    }
+  	    }
+      };,
     },
   };
 
