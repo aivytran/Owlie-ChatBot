44fcaa71bd287dac56db6f0475c4157b4d38ceff
aivytran <aivytran@berkeley.edu>
2017-04-06 10:37:58 -0700

    test

--- bot.js
+++ bot.js
@@ -44,7 +44,36 @@ const actions = {
       //     }
       //     delete response.quickreplies;
       // }
-      data = response
+      data = {
+  	    "attachment": {
+  		    "type": "template",
+  		    "payload": {
+  				"template_type": "generic",
+  			    "elements": [{
+  					"title": "First card",
+  				    "subtitle": "Element #1 of an hscroll",
+  				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+  				    "buttons": [{
+  					    "type": "web_url",
+  					    "url": "https://www.messenger.com",
+  					    "title": "web url"
+  				    }, {
+  					    "type": "postback",
+  					    "title": "Postback",
+  					    "payload": "Payload for first element in a generic bubble",
+  				    }],
+  			    }, {
+  				    "title": "Second card",
+  				    "subtitle": "Element #2 of an hscroll",
+  				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+  				    "buttons": [{
+  					    "type": "postback",
+  					    "title": "Postback",
+  					    "payload": "Payload for second element in a generic bubble",
+  				    }],
+  			    }]
+  		    }
+  	    };
       console.log(data);
       return FB.fbMessage(recipientId, data, (err, data) => {
         console.log("in Facebook");
