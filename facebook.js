a93379d502de2309039d5dca9a9a40dc92d0dad0
aivytran <aivytran@berkeley.edu>
2017-04-06 22:33:24 -0700

    set up welcome message

--- facebook.js
+++ facebook.js
@@ -8,7 +8,9 @@ const Config = require('./const.js');
 const createGreetingApi = (data) => {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/thread_settings',
-    qs: { access_token: Config.FB_PAGE_TOKEN },
+    qs: {
+      access_token: Config.FB_PAGE_TOKEN
+    },
     method: 'POST',
     json: data
 
