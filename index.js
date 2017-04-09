c280dc5d91a33d2e9ccabad3808f8685cbf9e3fb
Candra Tran <candratran@gmail.com>
2017-04-08 23:34:55 -0700

    testing out more suggestions

--- index.js
+++ index.js
@@ -157,6 +157,8 @@ app.post('/webhook', (req, res) => {
       );
     }
     else if (msg === "more suggestions") {
+      sessions[sessionId].context.itemPage += 1;
+      console.log(sessions[sessionId].context.itemPage);
       console.log("INSIDE MORE SUGGESTIONS FUNCTION!!");
       wit.runActions(
         sessionId, // the user's current session
