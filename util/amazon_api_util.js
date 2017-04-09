fa094163492cfb2ba022f0a3c5d33c542360627e
dorachin <chin22d@gmail.com>
2017-04-09 00:40:24 -0700

    Get shipping availability

--- util/amazon_api_util.js
+++ util/amazon_api_util.js
@@ -22,8 +22,10 @@ const searchItem = (keywords, itemPage, minimumPrice, maximumPrice) => {
   }).then(function(results){
     for (let i = 0; i < results.length; i++) {
       console.log(i);
-      // console.log(results[i]["Offers"]);
-      console.log(results[i]["ItemAttributes"][0]["ListPrice"]);
+      // console.log(results[i]);
+      // console.log(results[i]["Offers"][0]["Offer"][0]["OfferListing"][0]);
+      console.log(results[i]["Offers"][0]["Offer"][0]["OfferListing"][0]["Availability"][0]);
+      // console.log(results[i]["ItemAttributes"][0]["ListPrice"]);
       // console.log(results[i]["BrowseNodes"]);
     }
     return results;
